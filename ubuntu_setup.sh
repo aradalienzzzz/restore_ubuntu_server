20434586d21543c41860ff96433731758450de36
Aren Clegg <aclegg2011@gmail.com>
2017-03-14 19:33:32 -0700

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -10,5 +10,8 @@
 sudo rm -rfv systemd-timesyncd.service
 sudo apt install ntpdate
 
-# intel x52 chipset bug fix
+# intel x52 chipset bug fix (nano /etc/defaults/grub)
 GRUB_CMDLINE_LINUX_DEFAULT="iommu=no-intremap intel_iommu=off"
+
+# install nvidia driver after new kernel install
+ sh ./NVIDIA-Linux-x86_64-375.39.run
